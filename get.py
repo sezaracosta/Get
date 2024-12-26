7ba028fbebc05543f729f0955a6fe7352313b831
sezaracosta <sezara41@gmail.com>
2024-12-26 15:33:08 -0300

    ajustando if e else do codigo

--- get.py
+++ get.py
@@ -4,12 +4,7 @@ url = 'https://httpbin.org/status/404'
 request = requests.get(url)
 status = request.status_code
 
-if status >= 200:
-    if status == 200:
-        print('Sucesso')   
-    if status != 200:
-        print("sem conexão")
+if status == 200:
+    print("Sucesso")
 else:
-    print("Erro!")
-
-
+    print("erro Conexão")
